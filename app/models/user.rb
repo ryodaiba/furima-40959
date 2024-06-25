370535e3e63b12f19751adc2cda6c0f94ef6bbdb
ryodaiba <ryodaiba777@gmail.com>
2024-06-25 09:45:22 +0900

    ログイン・新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :dob, presence: true
+
+  has_many :items
+  has_many :purchases
+
 end
