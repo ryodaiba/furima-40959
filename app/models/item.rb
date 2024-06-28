b5529f627fb8c547cdb2d371a44d97b198e945fe
ryodaiba <ryodaiba777@gmail.com>
2024-06-28 18:02:08 +0900

    アソシエーション調整とエラーメッセージの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
@@ -7,7 +8,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   has_one_attached :image
 
-  validates :image, :item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
-  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image,           presence: true
+  validates :item_name,       presence: true
+  validates :description,     presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: 'cant be blank' }
+  validates :condition_id,    presence: true, numericality: { other_than: 1, message: 'cant be blank' }
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: 'cant be blank' }
+  validates :prefecture_id,   presence: true, numericality: { other_than: 1, message: 'cant be blank' }
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: 'cant be blank' }
+  validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
