f97b76137efe5a1f5e08556520adaab6a6188239
ryodaiba <ryodaiba777@gmail.com>
2024-07-05 08:58:16 +0900

    販売手数料および利益の自動計算JS

--- app/javascript/calculate_profit.js
+++ app/javascript/calculate_profit.js
@@ -1,10 +1,9 @@
-function calculateProfit() {
-  const price = document.getElementById('item-price').value;
-  const commission = Math.floor(price * 0.1);
-  const profit = Math.floor(price - commission);
-  document.getElementById('add-tax-price').textContent = commission;
-  document.getElementById('profit').textContent = profit;
-}
-
-
-window.addEventListener('turbo:load', item);
+window.addEventListener('turbo:load', () => {
+  function calculateProfit() {
+    const price = document.getElementById('item-price').value;
+    const commission = Math.floor(price * 0.1);
+    const profit = Math.floor(price - commission);
+    document.getElementById('add-tax-price').textContent = commission;
+    document.getElementById('profit').textContent = profit;
+  }
+});
