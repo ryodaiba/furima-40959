1609dcbb6f3ee70f361ef61cd22529281a37c6cf
ryodaiba <ryodaiba777@gmail.com>
2024-07-08 11:12:00 +0900

    PAYJPと接続した決済情報入力フォームのJS

---
+++ app/javascript/card.js
@@ -0,0 +1,50 @@
+window.addEventListener('turbo:load', () => {
+  console.log("turbo:loadイベントがトリガーされました。");
+
+  const pay = () => {
+    if (typeof window.payjpInstance === 'undefined') {
+      window.payjpInstance = Payjp('pk_test_ca801ca40543170e5de53b35'); // PAY.JPテスト公開鍵
+    }
+    const payjp = window.payjpInstance;
+    
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
+
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+
+    const form = document.getElementById('charge-form');
+    if (!form) {
+      console.log("フォームが見つかりませんでした。");
+      return;
+    }
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log("フォームが送信されました。");
+
+      payjp.createToken(numberElement).then(function (response) {
+        if (response.error) {
+          console.error("トークン作成中にエラーが発生しました：", response.error.message); // エラーメッセージをコンソールに表示
+          alert("カード情報が正しくありません。もう一度確認してください。");
+        } else {
+          const token = response.id;
+          console.log("トークンが作成されました：", token);
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+          console.log("トークンがフォームに追加されました。");
+        }
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.getElementById("charge-form").submit();
+      });
+    });
+  };
+
+  pay();
+});
