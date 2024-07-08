544c64dc11553098c15ba7058409e70ecfbfd551
ryodaiba <ryodaiba777@gmail.com>
2024-07-08 16:56:45 +0900

    バリデーションにuser_idとitem_idを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,6 +9,8 @@ class PurchaseAddress
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
     validates :token
+    validates :user_id
+    validates :item_id
   end
 
   def save
