163216b5409df67b103735e78d5e5217cfbb0d0b
ryodaiba <ryodaiba777@gmail.com>
2024-07-08 11:13:28 +0900

    tokenのバリデーション設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
@@ -8,6 +8,7 @@ class PurchaseAddress
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
+    validates :token
   end
 
   def save
