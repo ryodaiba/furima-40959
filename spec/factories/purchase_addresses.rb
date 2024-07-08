3cb71b24968894b4150d6784c93ec7bc165e4aa4
ryodaiba <ryodaiba777@gmail.com>
2024-07-08 16:57:11 +0900

    外部キーのFactoryBotは削除

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
-    user_id { FactoryBot.create(:user).id }
-    item_id { FactoryBot.create(:item).id}
   end
 end
